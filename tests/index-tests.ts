bf72425a1d02bbc534d80f27a4e570864d76bb54
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-09-08 12:57:58 +0300

    Testing typings for the new indexing (colons in the bracket operator)

--- tests/index-tests.ts
+++ tests/index-tests.ts
@@ -5,11 +5,14 @@ let fruits: PyArray<string> = pyarray(['Banana', 'Apple', 'Orange', 'Plum', 'Gra
 fruits.push('Avocado');
 fruits.pop();
 let array: string = fruits.toString();
-
 let all: Array<string> = fruits.get();
 let fruit: string = fruits.get(0);
 let some: Array<string> = fruits.get(0,-2);
+console.log(all, fruit, some);
+some = fruits['0:-1:2'];
 all = fruits.get(null, null, -1);
+all = fruits['::-1'];
+console.log(all, fruit, some);
 fruits.append('Avocado');
 fruits.extend(['Strawbarry', 'Blueberry']);
 fruits.insert(2, 'Coconut');
@@ -17,3 +20,4 @@ fruits.remove(2);
 fruit = fruits.pypop(2);
 let index: number = fruits.index('Plum');
 let count: number = fruits.count('Plum');
+console.log(all, fruit, some, index, count);
